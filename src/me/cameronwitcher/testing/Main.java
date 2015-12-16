97c1401708157eb538f0f866ed8d9df8a11cef51
Cameron Witcher <Cameron Witcher@HS-BUS-27.wellsville-usd289.org>
2015-12-16 12:49:16 -0600

    FIX

--- src/me/cameronwitcher/testing/Main.java
+++ src/me/cameronwitcher/testing/Main.java
@@ -1,16 +1,32 @@
 package me.cameronwitcher.testing;
 
-import javax.swing.JFrame;
+import java.io.StringReader;
 
-public class Main {
-	
-	public static void main(String[] args){
-		JFrame frame = new JFrame("Testing");
-		frame.pack();
-		frame.setSize(600, 600);
-		frame.setVisible(true);
-		Pane pane = new Pane();
-		frame.add(pane);
-	}
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.Provider;
+import javax.xml.ws.Service;
+import javax.xml.ws.ServiceMode;
+import javax.xml.ws.WebServiceProvider;
+import javax.xml.ws.http.HTTPBinding;
 
+@WebServiceProvider
+@ServiceMode(value = Service.Mode.PAYLOAD)
+public class Main implements Provider<Source> {
+
+    public Source invoke(Source request) {
+        return  new StreamSource(new StringReader("<p>Hello There!</p>"));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String address = "http://127.0.0.1:8080/";
+        Endpoint.create(HTTPBinding.HTTP_BINDING, new Main()).publish(address);
+
+//        System.out.println("Service running at " + address);
+//        System.out.println("Type [CTRL]+[C] to quit!");
+
+        Thread.sleep(Long.MAX_VALUE);
+    }
 }
