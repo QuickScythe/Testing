29ae65a00cd4e17730d39e1ba8358ab5fc7b3909
Cameron Witcher <Cameron Witcher@HS-BUS-27.wellsville-usd289.org>
2015-12-16 13:07:12 -0600

    Stuff

--- src/me/cameronwitcher/testing/Main.java
+++ src/me/cameronwitcher/testing/Main.java
@@ -1,33 +1,31 @@
 package me.cameronwitcher.testing;
 
-import java.io.StringReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.ws.Endpoint;
-import javax.xml.ws.Provider;
-import javax.xml.ws.Service;
-import javax.xml.ws.ServiceMode;
-import javax.xml.ws.WebServiceProvider;
-import javax.xml.ws.http.HTTPBinding;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-@WebServiceProvider
-@ServiceMode(value = Service.Mode.PAYLOAD)
-public class Main implements Provider<Source> {
+public class Main {
 
-    public Source invoke(Source request) {
-        return  new StreamSource(new StringReader("<p>Hello There!</p>"));
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
-    
 
-    public static void main(String[] args) throws InterruptedException {
-
-        String address = "http://127.0.0.1:8080/";
-        Endpoint.create(HTTPBinding.HTTP_BINDING, new Main()).publish(address);
-
-//        System.out.println("Service running at " + address);
-//        System.out.println("Type [CTRL]+[C] to quit!");
-
-        Thread.sleep(Long.MAX_VALUE);
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
+
 }
