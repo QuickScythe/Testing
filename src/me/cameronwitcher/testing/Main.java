8126e9b0b4678f94b611a451e9b4c9d733395546
Cameron Witcher <CameronWitcher2017@gmail.com>
2015-12-22 12:22:39 -0600

    Testing args in Travis

--- src/me/cameronwitcher/testing/Main.java
+++ src/me/cameronwitcher/testing/Main.java
@@ -5,11 +5,13 @@ import javax.swing.JFrame;
 public class Main {
 	
 	public static void main(String[] args){
-		JFrame frame = new JFrame("TEST");
-		frame.pack();
-		frame.setVisible(true);
-		frame.setSize(400, 400);
-		
+		if(args[0].equalsIgnoreCase("testing")){
+			for(int x=0;x!=641;x++){
+				for(int y=0;y!=641;y++){
+					System.out.println(x + ":" + y);
+				}
+			}
+		}
 		
 	}
 }
