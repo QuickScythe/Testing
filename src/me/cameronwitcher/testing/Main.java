b0d7ffd6e95ec664265e6c46094de4f4d2461173
Cameron Witcher <CameronWitcher2017@gmail.com>
2016-01-11 12:14:59 -0600

    CHANGE: Made numbers smaller

--- src/me/cameronwitcher/testing/Main.java
+++ src/me/cameronwitcher/testing/Main.java
@@ -1,13 +1,11 @@
 package me.cameronwitcher.testing;
 
-import javax.swing.JFrame;
-
 public class Main {
 	
 	public static void main(String[] args){
 		if(args[0].equalsIgnoreCase("testing")){
-			for(int x=0;x!=641;x++){
-				for(int y=0;y!=641;y++){
+			for(int x=0;x!=64;x++){
+				for(int y=0;y!=64;y++){
 					System.out.println(x + ":" + y);
 				}
 			}
